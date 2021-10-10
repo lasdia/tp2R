bbcd112a33f7770c366628d165575446f0b1fab9
lasdia <l.diallo0@laposte.net>
2021-10-11 00:00:04 +0200

    CRUD Utilisateur

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use App\Repository\UtilisateurRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/utilisateur")
+ */
+class UtilisateurController extends AbstractController
+{
+    /**
+     * @Route("/", name="utilisateur_index", methods={"GET"})
+     */
+    public function index(UtilisateurRepository $utilisateurRepository): Response
+    {
+        return $this->render('utilisateur/index.html.twig', [
+            'utilisateurs' => $utilisateurRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="utilisateur_new", methods={"GET","POST"})
+     */
+    public function new(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $utilisateur->setPassword(
+             $userPasswordHasherInterface->hashPassword(
+                 $utilisateur,
+                 $form->get('password')->getData()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('utilisateur_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('utilisateur/new.html.twig', [
+            'utilisateur' => $utilisateur,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="utilisateur_show", methods={"GET"})
+     */
+    public function show(Utilisateur $utilisateur): Response
+    {
+        return $this->render('utilisateur/show.html.twig', [
+            'utilisateur' => $utilisateur,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="utilisateur_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Utilisateur $utilisateur,UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    {
+
+        $ancien_mdp = $utilisateur->getPassword();
+
+        $utilisateur->setPassword('******');
+
+
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if($form->get('password')->getData()!='******'){
+                $utilisateur->setPassword(
+                    $userPasswordHasherInterface->hashPassword(
+                        $utilisateur,
+                        $form->get('password')->getData()
+                       )
+                   );
+            } else{
+                $utilisateur->setPassword($ancien_mdp);
+            }
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('utilisateur_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('utilisateur/edit.html.twig', [
+            'utilisateur' => $utilisateur,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="utilisateur_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Utilisateur $utilisateur): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$utilisateur->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($utilisateur);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('utilisateur_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
